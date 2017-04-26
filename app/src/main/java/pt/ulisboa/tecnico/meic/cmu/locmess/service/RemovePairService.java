41e71dfab5bfd867b5d4f67fbc653e52a2409118
drlferreira <diogo.lopes.ferreira@tecnico.ulisboa.pt>
2017-04-26 23:26:23 +0100

    Pair Service functionality added!

---
+++ app/src/main/java/pt/ulisboa/tecnico/meic/cmu/locmess/service/RemovePairService.java
@@ -0,0 +1,51 @@
+package pt.ulisboa.tecnico.meic.cmu.locmess.service;
+
+import android.content.Context;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.entity.StringEntity;
+import pt.ulisboa.tecnico.meic.cmu.locmess.R;
+import pt.ulisboa.tecnico.meic.cmu.locmess.dto.Pair;
+import pt.ulisboa.tecnico.meic.cmu.locmess.handler.LocmessRestHandler;
+import pt.ulisboa.tecnico.meic.cmu.locmess.interfaces.ActivityCallback;
+import pt.ulisboa.tecnico.meic.cmu.locmess.interfaces.LocmessCallback;
+
+/**
+ * Created by Diogo on 26/04/2017.
+ */
+
+public class RemovePairService extends LocmessWebService implements LocmessCallback {
+
+    private Pair pair;
+
+    public RemovePairService(Context context, ActivityCallback activityCallback, Pair pair) {
+        super(context, activityCallback);
+        this.pair = pair;
+    }
+
+    @Override
+    protected void dispatch() {
+        String pair = new JsonService().transformObjToJson(this.pair);
+        String endpoint = getContext().getString(R.string.webserver_endpoint_user_removepair);
+        String contentType = getContext().getString(R.string.content_type_json);
+        try {
+            getHttpService().post(endpoint, new StringEntity(pair), contentType, new LocmessRestHandler(this));
+        } catch (UnsupportedEncodingException ignored) {
+        }
+    }
+
+    @Override
+    public void onSuccess(JSONObject object) {
+        System.out.println(object.toString());
+    }
+
+    @Override
+    public void onFailure(JSONObject object) {
+        System.out.println(object.toString());
+    }
+
+
+}
