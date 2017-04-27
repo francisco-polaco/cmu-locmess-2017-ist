d529a40cfc195714911a2a8312632cf74e6d99ca
drlferreira <diogo.lopes.ferreira@tecnico.ulisboa.pt>
2017-04-27 13:13:55 +0100

    Location Hierarchy added

---
+++ app/src/main/java/pt/ulisboa/tecnico/meic/cmu/locmess/dto/Location.java
@@ -0,0 +1,22 @@
+package pt.ulisboa.tecnico.meic.cmu.locmess.dto;
+
+/**
+ * Created by Diogo on 27/04/2017.
+ */
+
+public class Location  {
+
+    private String name;
+
+    public Location(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
