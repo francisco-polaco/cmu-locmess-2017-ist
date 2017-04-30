39b2c54f27190e12f8d83a79fb81bc0d449e3b43
Radaeld <joao.serras@ist.utl.pt>
2017-04-30 19:45:32 +0100

    WifiDirect

--- app/src/main/java/pt/ulisboa/tecnico/meic/cmu/locmess/presentation/WifiLocationPicker.java
+++ app/src/main/java/pt/ulisboa/tecnico/meic/cmu/locmess/presentation/WifiLocationPicker.java
@@ -1,18 +1,26 @@
 package pt.ulisboa.tecnico.meic.cmu.locmess.presentation;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
@@ -20,8 +28,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import pt.inesc.termite.wifidirect.SimWifiP2pDevice;
+import pt.inesc.termite.wifidirect.SimWifiP2pDeviceList;
+import pt.inesc.termite.wifidirect.SimWifiP2pManager;
+import pt.inesc.termite.wifidirect.service.SimWifiP2pService;
 import pt.ulisboa.tecnico.meic.cmu.locmess.R;
 import pt.ulisboa.tecnico.meic.cmu.locmess.domain.God;
+import pt.ulisboa.tecnico.meic.cmu.locmess.domain.SimWifiP2pBroadcastReceiver;
 import pt.ulisboa.tecnico.meic.cmu.locmess.googleapi.GoogleAPI;
 import pt.ulisboa.tecnico.meic.cmu.locmess.interfaces.ActivityCallback;
 
@@ -29,14 +42,15 @@ import pt.ulisboa.tecnico.meic.cmu.locmess.interfaces.ActivityCallback;
  * Created by jp_s on 4/27/2017.
  */
 
-public class WifiLocationPicker extends AppCompatActivity {
+public class WifiLocationPicker extends AppCompatActivity implements
+        SimWifiP2pManager.PeerListListener {
 
         private static final String TAG = pt.ulisboa.tecnico.meic.cmu.locmess.presentation.WifiLocationPicker.class.getSimpleName();
         private Toolbar toolbar;
-        WifiManager wifi;
-        BroadcastReceiver broadcastreceiver;
-        List<ScanResult> results = wifi.getScanResults();
-        private int size;
+
+        private SimWifiP2pManager mManager = null;
+        private SimWifiP2pManager.Channel mChannel = null;
+        private SimWifiP2pBroadcastReceiver mReceiver;
 
 
     @Override
@@ -47,51 +61,19 @@ public class WifiLocationPicker extends AppCompatActivity {
             toolbar = (Toolbar) findViewById(R.id.toolbar);
             setSupportActionBar(toolbar);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-
-            wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            if (wifi.isWifiEnabled() == false)
-            {
-                Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
-                wifi.setWifiEnabled(true);
-            }
-
-            broadcastreceiver = new BroadcastReceiver()
-            {
-                @Override
-                public void onReceive(Context c, Intent intent)
-                {
-                    results = wifi.getScanResults();
-                    size = results.size();
-                }
-            };
-            getApplicationContext().registerReceiver(broadcastreceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
-            getApplicationContext().unregisterReceiver(broadcastreceiver);
     }
 
 
     public void onStart(View view)
     {
-        wifi.startScan();
+        Intent intent = new Intent(getApplicationContext(), SimWifiP2pService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        mManager.requestPeers(mChannel, this);
+    }
 
-        Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
-        try
-        {
-            size = size - 1;
-            while (size >= 0)
-            {
-                HashMap<String, String> item = new HashMap<String, String>();
-                item.put(ITEM_KEY, results.get(size).SSID + "  " + results.get(size).capabilities);
-
-                arraylist.add(item);
-                size--;
-                adapter.notifyDataSetChanged();
-            }
-        }
-        catch (Exception e)
-        { }
+    public void onStop(View view)
+    {
+        unbindService(mConnection);
     }
 
     @Override
@@ -113,5 +95,39 @@ public class WifiLocationPicker extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onPeersAvailable(SimWifiP2pDeviceList peers) {
+        ArrayList<String> peersStr = new ArrayList<>();
+
+        // compile list of devices in range
+        for (SimWifiP2pDevice device : peers.getDeviceList()) {
+            String devstr =device.deviceName + " (" + device.getVirtIp() + ")";
+            peersStr.add(devstr);
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, peersStr);
+
+        ListView listview= (ListView) findViewById(R.id.wifi_list);
+        listview.setAdapter(adapter);
+        //TODO: ListAdaptar
+
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        // callbacks for service binding, passed to bindService()
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mManager = new SimWifiP2pManager(new Messenger(service));
+            mChannel = mManager.initialize(getApplication(), getMainLooper(), null);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mManager = null;
+            mChannel = null;
+        }
+    };
 }
 
